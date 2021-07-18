e9512f609fb6600e5dcb0579f53107ffd44eae1d
jonathas aroucha <jonathasfelipe_aroucha@hotmail.com>
2021-07-18 17:37:48 -0300

    :package: sessão conhecimentos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 document.querySelector(".hamburguer").addEventListener("click", function(){
-     document.querySelector(".sidebar").classList.toggle("show-menu")
+     // document.querySelector(".sidebar").classList.toggle("show-menu")
      document.querySelector(".container").classList.toggle("show-menu")
      
      
