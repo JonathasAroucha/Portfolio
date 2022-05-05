c87af9e5dad2e1e8163ac6093b8d8ef160ca56a1
jonathasaroucha <jonathasfelipe_aroucha@hotmail.com>
2022-05-05 19:05:33 -0300

    ajustando script de botão hamburguer

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 document.querySelector(".hamburguer").addEventListener("click", function () {
-     // document.querySelector(".sidebar").classList.toggle("show-menu")
-     document.querySelector(".container").classList.toggle("show-menu")
+    document.querySelector(".container").classList.add("show-menu")
 })
-
-
+document.querySelector(".menu").addEventListener("click", function () {
+		document.querySelector(".container").classList.remove("show-menu")
+})
 
