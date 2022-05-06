2ebaddca5dc4d306d72095f701006497d09ac707
jonathasaroucha <jonathasfelipe_aroucha@hotmail.com>
2022-05-05 22:27:29 -0300

    ajuste final de responsividade e botão hamburguer

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let hamburguer = document.querySelector(".hamburguer")
 let container = document.querySelector(".container")
 let menu = document.querySelector(".menu")
 
+
 hamburguer.addEventListener("click", function () {
-    container.classList.add("show-menu")
+    container.classList.toggle("show-menu")
 })
 
 menu.addEventListener("click", function () {
-        container.classList.remove("show-menu")
+    container.classList.toggle("show-menu")
 })
 
